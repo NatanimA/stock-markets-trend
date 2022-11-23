66bfd157cbd44689dc63886877f9e979f203e10d
natanimabesha <natanimabesha@www.zhewutech.com>
2022-11-23 23:04:53 +0300

    Fix image error

--- src/components/StockItems.js
+++ src/components/StockItems.js
@@ -3,13 +3,20 @@ import { FaLongArrowAltDown, FaLongArrowAltUp } from 'react-icons/fa';
 import { FiArrowRightCircle } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Chart from '../assets/images/chart.jpg';
 
 const StockItems = ({ stock }) => {
   const {
     symbol, name, price, change, changesPercentage,
   } = stock;
-  // eslint-disable-next-line
-  const imgBG = require(`../assets/company/${symbol}.png`);
+  let imgBG = null;
+  try {
+    // eslint-disable-next-line
+    imgBG = require(`../assets/company/${symbol}.png`);
+  } catch (err) {
+    imgBG = Chart;
+  }
+
   return (
     <Card className="card-container">
       <Link to={`details/${symbol}`} key={`${symbol}link`} className="text-white">
